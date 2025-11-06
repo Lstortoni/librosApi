06c71ec61ed5653d103a471aa5d4b70bf6f04455
Lstortoni <89618746+Lstortoni@users.noreply.github.com>
2025-11-06 18:14:03 -0300

    Finished withs EF Settings

--- ApiLibrosController/Program.cs
+++ ApiLibrosController/Program.cs
@@ -3,13 +3,36 @@ using ApiLibrosController.Infrastructure;
 using ApiLibrosController.Interfaces.Jwt;
 using ApiLibrosController.Interfaces.Repositories;
 using ApiLibrosController.Interfaces.Services;
+using ApiLibrosController.Repository.Data;
 using ApiLibrosController.Repository.FakeImplementation;
 using ApiLibrosController.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Npgsql.EntityFrameworkCore.PostgreSQL;                 // UseNpgsql
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+//Entity Framework Core - Postgres
+
+// Configurar el DbContext con PostgreSQL
+// Connection string (appsettings o env var)
+var cs = builder.Configuration.GetConnectionString("DefaultConnection")
+         ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+// EF Core + Npgsql
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(cs, x =>
+    {
+        // x.MigrationsHistoryTable("__EFMigrationsHistory", "public"); // opcional
+    })
+    .UseSnakeCaseNamingConvention(); // opcional, prolijo en Postgres
+});
 
 // Add services to the container.
 
@@ -19,7 +42,7 @@ builder.Services.AddControllers();
 builder.Services.Configure<JwtOptions>(
     builder.Configuration.GetSection("Jwt"));
 // Services
-builder.Services.AddScoped<IVendedorService, VendedorService>();
+
 builder.Services.AddScoped<ILibroService, LibroService>();
 
 
@@ -30,7 +53,7 @@ builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 
 
 // Repos InMemory (para probar YA)
-builder.Services.AddSingleton<IVendedorRepository, VendedorRepositoryInMemory>();
+
 builder.Services.AddSingleton<ILibroRepository, LibroRepositoryInMemory>();
 
 builder.Services.AddEndpointsApiExplorer();
